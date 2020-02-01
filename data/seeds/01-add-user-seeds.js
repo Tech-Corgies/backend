fdb0238f980a08f5e6320ac4983c59eca2b87778
richanynguon <alina.nguon@gmail.com>
2020-01-31 20:55:28 -0500

    create user table and seeds

--- data/seeds/01-add-user-seeds.js
+++ data/seeds/01-add-user-seeds.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const cryptoRandomString = require("crypto-random-string");
+const uuid = require("uuid/v4");
 
 exports.seed = function(knex) {
   return knex("users")
@@ -7,26 +8,31 @@ exports.seed = function(knex) {
     .then(function() {
       return knex("users").insert([
         {
+          id: uuid.v4(),
           email: "amy",
           password: bcrypt.hashSync("amy", 10),
           password_salt: cryptoRandomString({ length: 10 })
         },
         {
+          id: uuid.v4(),
           email: "isabel@email.com",
           password: bcrypt.hashSync("isabel", 10),
           password_salt: cryptoRandomString({ length: 10 })
         },
         {
+          id: uuid.v4(),
           email: "pooja@email.com",
           password: bcrypt.hashSync("pooja", 10),
           password_salt: cryptoRandomString({ length: 10 })
         },
         {
+          id: uuid.v4(),
           email: "ritu@email.com",
           password: bcrypt.hashSync("ritu", 10),
           password_salt: cryptoRandomString({ length: 10 })
         },
         {
+          id: uuid.v4(),
           email: "richany@email.com",
           password: bcrypt.hashSync("richany", 10),
           password_salt: cryptoRandomString({ length: 10 })
